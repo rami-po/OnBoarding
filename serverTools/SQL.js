db493ce4556d19f2c01f48b169222cb74f774cb8
rami-po <rami.khadder@productops.com>
2017-09-26 09:42:46 -0700

    SQL disconnects are now handled

--- serverTools/SQL.js
+++ serverTools/SQL.js
@@ -4,13 +4,39 @@
 var mysql = require('mysql');
 var sqlSecret = require('./secrets/sql_secret.json');
 
-var connection = mysql.createConnection({
+var sql_config = {
   host: sqlSecret.HOST,
   port: sqlSecret.PORT,
   user: sqlSecret.MYSQL_USER,
   password: sqlSecret.MYSQL_PASS,
-  database: sqlSecret.DATABASE
-});
+  database: sqlSecret.DATABASE,
+  multipleStatements: true
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(sql_config);
+
+  connection.connect(function (err) {              // The server is either down
+    if (err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function (err) {
+    console.log('db error', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
 
 exports.getUser = function(user, onPostExecute){
   connection.query("SELECT * FROM admin WHERE user='" + user + "'", function(err, result){
